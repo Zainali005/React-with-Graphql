67ebb9c3379d92ccdd94cea744786fc0b5d7a508
Zainali005 <zainali5002@gmail.com>
2024-07-24 14:06:00 -0400

    graphql done

---
+++ src/main.tsx
@@ -0,0 +1,16 @@
+// const client = ...
+
+client
+  .query({
+    query: gql`
+      query GetLocations {
+        locations {
+          id
+          name
+          description
+          photo
+        }
+      }
+    `,
+  })
+  .then((result) => console.log(result));
